3f4baf10480280833ba5d39af5445bfdf4e94837
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2021-10-31 15:37:40 +0100

    commenter et organiser le code

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,5 @@
-const produit = document.querySelector(".cart__item ");
-// const image = document.querySelector(".cart__item__img");
+const produitContent = document.querySelector("#cart__items");
+const image = document.querySelector(".cart__item__img");
 const name = document.querySelector(".cart__item__content__titlePrice ");
 const price = document.querySelector(".cart__item__content__titlePrice , p");
 const quantite = document.querySelector(
@@ -24,78 +24,41 @@ function initialiserPanier() {
   let panier = JSON.parse(res ? res : "[]");
   let total = 0;
   let totalQte = 0;
+  produitContent.innerHTML = "";
+  // total = article.quantite * achat.produit.price;
+  panier.forEach((article) => {
+    produitContent.innerHTML += `<article class="cart__item" data-id=${article.produit._id} data-couleur=${article.couleur}>
+<div class="cart__item__img">
+  <img src=${article.produit.imageUrl} alt=${article.produit.altTxt}> 
+ </div>
+<div class="cart__item__content">
+  <div class="cart__item__content__titlePrice">
+    <h2>${article.produit.name}</h2>
+    <h3>${article.couleur}</h3>
 
-  let cart__items = document.getElementById("cart__items");
-  // on vide les balises enfants de cart_items
-  cart__items.innerHTML = "";
-  // total = achat.quantite * achat.produit.price;
-  panier.forEach((achat) => {
-    let article = document.createElement("article");
-    article.setAttribute("class", "cart__item");
-    article.setAttribute("data-id", achat.produit._id);
-    article.setAttribute("data-couleur", achat.couleur);
-
-    let imageDiv = document.createElement("div");
-    imageDiv.setAttribute("class", "cart__item__img");
-    article.appendChild(imageDiv);
-    let imageNode = document.createElement("img");
-    imageNode.src = achat.produit.imageUrl;
-    imageNode.alt = achat.produit.altTxt;
-    imageDiv.appendChild(imageNode);
-
-    let titleDiv1 = document.createElement("div");
-    titleDiv1.setAttribute("class", "cart__item__content");
-    article.appendChild(titleDiv1);
-    let titleDiv2 = document.createElement("div");
-    titleDiv2.setAttribute("class", "cart__item__content__titlePrice");
-    titleDiv1.appendChild(titleDiv2);
-    let titleH2 = document.createElement("h2");
-    titleH2.textContent = achat.produit.name + "(" + achat.couleur + ")";
-    titleDiv2.appendChild(titleH2);
-    let textP = document.createElement("p");
-    textP.textContent = achat.produit.price + "€";
-    titleDiv2.appendChild(textP);
-
-    let settings = document.createElement("div");
-    settings.setAttribute("class", "cart__item__content__settings");
-    article.appendChild(settings);
-    let quantite = document.createElement("div");
-    quantite.setAttribute("class", "cart__item__content__settings__quantity");
-
-    settings.appendChild(quantite);
-    let textQuantite = document.createElement("p");
-    textQuantite.textContent = "Qte:";
-    quantite.appendChild(textQuantite);
-    let input = document.createElement("input");
-    input.setAttribute("type", "number");
-    input.setAttribute("class", "itemQuantity");
-    input.setAttribute("name", "itemQuantity");
-    input.setAttribute("min", "1");
-    input.setAttribute("max", "100");
-    input.setAttribute("value", achat.quantite);
-    quantite.appendChild(input);
-    /////modifier la quantite dans le panier
-
-    let delet = document.createElement("div");
-    delet.setAttribute("class", "cart__item__content__settings__delete");
-    settings.appendChild(delet);
-    let textDelete = document.createElement("p");
-    textDelete.setAttribute("class", "deleteItem");
-    delet.appendChild(textDelete);
-    cart__items.appendChild(article);
-    textDelete.textContent = "supprimer";
-    textDelete.addEventListener("click", (e) => {
-      removeArticle(e, panier);
-    });
-
-    let valeur = achat.quantite * achat.produit.price;
+    <p>${article.produit.price} €</p>
+  </div>
+  <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+      <p>Qté : </p>
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${article.quantite}>
+    </div>
+    <div class="cart__item__content__settings__delete">
+      <p class="deleteItem">Supprimer</p>
+    </div>
+  </div>
+</div>
+</article>`;
+    let valeur = article.quantite * article.produit.price;
     total += valeur;
-    totalQte += parseInt(achat.quantite);
+    totalQte += parseInt(article.quantite);
     let totalQuantity = document.getElementById("totalQuantity");
     totalQuantity.textContent = totalQte;
     let totalPrice = document.getElementById("totalPrice");
     totalPrice.textContent = total;
-
+  });
+  const inputQty = document.querySelectorAll(".itemQuantity");
+  inputQty.forEach((input) => {
     input.addEventListener("change", (e) => {
       total = 0;
       // on récupère la nouvelle valeur quanité
@@ -105,24 +68,29 @@ function initialiserPanier() {
       let idProduit = articleBalise.getAttribute("data-id");
       let couleurChoisi = articleBalise.getAttribute("data-couleur");
 
-      // on cherche l'achat dans le panier
-      modifPanier = panier.find(
-        (achat) =>
-          achat.produit._id == idProduit && achat.couleur == couleurChoisi
+      // on cherche l'article dans le panier
+      let articleTrouve = panier.find(
+        (article) =>
+          article.produit._id == idProduit && article.couleur == couleurChoisi
       );
-      // on va modifier la valeur qty du modifP
-      modifPanier.quantite = newQtyValue;
-      // on sevaugarde le panier
+      // on va modifier la valeur qty de l'articleTrouve
+      articleTrouve.quantite = newQtyValue;
+      // on enregistre les modifications du panier
       localStorage.panier = JSON.stringify(panier);
       //reinitialiser les infos de la page
       initialiserPanier();
-
-      console.log(achat.quantite);
+    });
+  });
+  /////Supprimer un article à partir du panier
+  const deleteBtns = document.querySelectorAll(".deleteItem");
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener("click", (e) => {
+      removeArticle(e, panier);
     });
   });
 }
 
-//validation formulaire
+///////////////validation formulaire
 function ValidationMail(e) {
   var regex1 =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -163,7 +131,7 @@ let nameLast = document.querySelector("input#lastName");
 let adresse = document.querySelector("input#address");
 let city = document.querySelector("input#city");
 let email = document.querySelector("input#email");
-
+//envoyer la commande vers le back
 function send(e) {
   res = localStorage.panier;
   let panier = JSON.parse(res ? res : "[]");
@@ -213,14 +181,13 @@ function removeArticle(e, panier) {
   let id = articleBalise.getAttribute("data-id");
   let couleurChoisi = articleBalise.getAttribute("data-couleur");
 
-  newPanier = panier.filter(
+  //on filtre que les produits qui n'ont pas la meme couleur ou   qui n'ont pas le meme id. c'est a dire on supprime les produits qui ont le meme id et la meme couleur.
+  panierFiltre = panier.filter(
     (achat) => achat.produit._id != id || achat.couleur != couleurChoisi
   );
-  localStorage.panier = JSON.stringify(newPanier);
+  localStorage.panier = JSON.stringify(panierFiltre);
   initialiserPanier();
 }
 
 //code execution here:
 initialiserPanier();
-let a = 0;
-console.log(a);
--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,4 @@
-let itemImage = document.querySelector(".item .item__img");
+let image = document.querySelector(".item .item__img");
 let name = document.getElementById("title");
 let prix = document.getElementById("price");
 let description = document.getElementById("description");
@@ -8,9 +8,32 @@ let card = document.getElementById("addToCart");
 let quantite = "";
 let product;
 
-function getProduct() {
+/**Récuperer get parameteres **/
+function getIdFromParam() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get("id");
+}
+
+function injectProduit(kanape) {
+  image.innerHTML = `<img src=${kanape.imageUrl} alt=${kanape.altTxt}>`;
+  name.textContent = kanape.name;
+  prix.textContent = kanape.price;
+  description.textContent = kanape.description;
+  kanape.colors.forEach((color) => {
+    // on crée l'option à partir de la couleur
+    let option = document.createElement("option");
+    option.textContent = color;
+    option.value = color;
+    // on attache l'option créer avec Select colors element
+    colors.appendChild(option);
+    product = kanape;
+  });
+}
+
+function afficherProduit() {
   const id = getIdFromParam();
-  //   console.log(id);
+  // si l'id n'et dans URLSearchParams on annule et  on sort
   if (!id) {
     console.log("no id found in parameters");
     return;
@@ -25,54 +48,35 @@ function getProduct() {
   };
   fetch("http://localhost:3000/api/products/" + id, init)
     .then((res) => res.json())
-    .then((kanape) => {
-      let imageNode = document.createElement("img");
-      imageNode.src = kanape.imageUrl;
-      imageNode.alt = imageNode.altTxt;
-      itemImage.appendChild(imageNode);
-      name.textContent = kanape.name;
-      prix.textContent = kanape.price;
-      description.textContent = kanape.description;
-      kanape.colors.forEach((color) => {
-        // on crée l'option à partir de la couleur
-        let option = document.createElement("option");
-        option.textContent = color;
-        option.value = color;
-        // on attache l'option créer avec Select colors element
-        colors.appendChild(option);
-        product = kanape;
-      });
-    });
+    .then(injectProduit);
+  //OnClick
   card.addEventListener("click", (e) => {
     const qty = inputQuantite.value;
     const valeurCouleur = colors.value;
+    //on récupère  le panier depuis localStorage
     res = localStorage.panier;
+    //si le panier est encore vide on initialise la var panier par un tableau vide  (ternary operator)
     let panier = JSON.parse(res ? res : "[]");
-    let achatTrouve = panier.find(
+    let articleTrouve = panier.find(
       (a) => a.produit._id == product._id && a.couleur == valeurCouleur
     );
-    if (!achatTrouve) {
-      //si achatTrouve est vide
-      let achat = {
+    if (!articleTrouve) {
+      //si articleTrouve est vide on crée un nouvel article
+      let article = {
         quantite: parseInt(qty),
         couleur: valeurCouleur,
         produit: product,
       };
-      panier.push(achat);
+      panier.push(article);
     } else {
-      achatTrouve.quantite += parseInt(qty);
+      //sinon on modifie l'article existant en ajoutant la quantité saisie
+      articleTrouve.quantite += parseInt(qty);
     }
+    //Enregistrer le panier dans localStroage
     localStorage.panier = JSON.stringify(panier);
-
+    // on se redirige vers la page d'accueil
     window.location = "index.html";
   });
 }
 
-getProduct();
-
-function getIdFromParam() {
-  /**Récuuper get parameteres **/
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get("id");
-}
+afficherProduit();
