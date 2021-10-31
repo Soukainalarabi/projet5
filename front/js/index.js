cefa09925ccff71c49c969a589d6ef3da3232f6d
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2021-10-31 14:45:52 +0100

    page d'accueil ok

--- front/js/index.js
+++ front/js/index.js
@@ -1,18 +1,20 @@
 const contentProducts = document.querySelector("section");
-///Afficher tous les produits///
+///Récupérer tous les produits///
 async function afficherProduits() {
   await fetch("http://localhost:3000/api/products/")
     .then((res) => res.json())
-    .then((kanapes) => {
-      kanapes.forEach((kn) => {
-        contentProducts.innerHTML += `<a href="./product.html?id=${kn._id}">
-        <article>
-          <img src=${kn.imageUrl} alt=${kn.altTxt}>
-          <h3 class="productName">${kn.name}</h3>
-          <p class="productDescription">${kn.description}</p>
-        </article>
-      </a> `;
-      });
-    });
+    .then(injectProduits);
+}
+////injecter les produits dans le HTML
+function injectProduits(kanapes) {
+  kanapes.forEach((kn) => {
+    contentProducts.innerHTML += `<a href="./product.html?id=${kn._id}">
+    <article>
+      <img src=${kn.imageUrl} alt=${kn.altTxt}>
+      <h3 class="productName">${kn.name}</h3>
+      <p class="productDescription">${kn.description}</p>
+    </article>
+  </a> `;
+  });
 }
 afficherProduits();
